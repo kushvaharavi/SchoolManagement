50085ad42d069e686efb84232555fc3c9a4b6940
Ravi Raj Kushvaha <ravirajkushvaha@gmail.com>
2020-04-05 12:17:38 +0530

    removing all APIs from the master branch to merge all the codes from the all the branches

--- SchoolManagement/src/main/java/com/demo/schoolmanagement/controller/StudentController.java
+++ SchoolManagement/src/main/java/com/demo/schoolmanagement/controller/StudentController.java
@@ -1,76 +1,10 @@
 package com.demo.schoolmanagement.controller;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.demo.schoolmanagement.exception.ResourceNotFoundException;
-import com.demo.schoolmanagement.model.Student;
-import com.demo.schoolmanagement.repository.StudentRepository;
 
 @RestController
 @RequestMapping("/api/v1")
 public class StudentController {
  
-    @Autowired
-    private StudentRepository studentRepository;
-
-    @GetMapping("/students")
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
-
-    @GetMapping("/students/{rollNo}")
-    public ResponseEntity<Student> getStudentById(
-    @PathVariable(value = "rollNo") Long rollNo) throws ResourceNotFoundException {
-    	Student student = studentRepository.findById(rollNo)
-        .orElseThrow(() -> new ResourceNotFoundException("Student not found on :: "+ rollNo));
-        return ResponseEntity.ok().body(student);
-    }
-
-    @PostMapping("/students")
-    public Student createStudent(@Valid @RequestBody Student student) {
-        return studentRepository.save(student);
-    }
-
-    @PutMapping("/students/{rollNo}")
-    public ResponseEntity<Student> updateStudent(
-    @PathVariable(value = "rollNo") Long rollNo,
-    @Valid @RequestBody Student studentDetails) throws ResourceNotFoundException {
-    	Student student = studentRepository.findById(rollNo)
-          .orElseThrow(() -> new ResourceNotFoundException("Student not found on :: "+ rollNo));
-  
-    	student.setEmailId(studentDetails.getEmailId());
-    	student.setLastName(studentDetails.getLastName());
-    	student.setFirstName(studentDetails.getFirstName());
-    	student.setUpdatedAt(new Date());
-        final Student updatedStudent = studentRepository.save(student);
-        return ResponseEntity.ok(updatedStudent);
-   }
-
-   @DeleteMapping("/student/{rollNo}")
-   public Map<String, Boolean> deleteStudent(
-       @PathVariable(value = "rollNo") Long rollNo) throws Exception {
-	   Student student = studentRepository.findById(rollNo)
-          .orElseThrow(() -> new ResourceNotFoundException("Student not found on :: "+ rollNo));
-
-	   studentRepository.delete(student);
-       Map<String, Boolean> response = new HashMap<>();
-       response.put("deleted", Boolean.TRUE);
-       return response;
-   }
 }
